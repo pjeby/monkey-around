65cb513a895fce019a8bf84606d1260569bc7128
PJ <pje@telecommunity.com>
2021-03-15 02:58:23 -0400

    Add ESM module support

---
+++ mjs/index.js
@@ -0,0 +1,53 @@
+export function around(obj, factories) {
+    const removers = Object.keys(factories).map(key => around1(obj, key, factories[key]));
+    return removers.length === 1 ? removers[0] : function () { removers.forEach(r => r()); };
+}
+function around1(obj, method, createWrapper) {
+    const original = obj[method], hadOwn = obj.hasOwnProperty(method);
+    let current = createWrapper(original);
+    // Let our wrapper inherit static props from the wrapping method,
+    // and the wrapping method, props from the original method
+    if (original)
+        Object.setPrototypeOf(current, original);
+    Object.setPrototypeOf(wrapper, current);
+    obj[method] = wrapper;
+    // Return a callback to allow safe removal
+    return remove;
+    function wrapper(...args) {
+        // If we have been deactivated and are no longer wrapped, remove ourselves
+        if (current === original && obj[method] === wrapper)
+            remove();
+        return current.apply(this, args);
+    }
+    function remove() {
+        // If no other patches, just do a direct removal
+        if (obj[method] === wrapper) {
+            if (hadOwn)
+                obj[method] = original;
+            else
+                delete obj[method];
+        }
+        if (current === original)
+            return;
+        // Else pass future calls through, and remove wrapper from the prototype chain
+        current = original;
+        Object.setPrototypeOf(wrapper, original || Function);
+    }
+}
+export function after(promise, cb) {
+    return promise.then(cb, cb);
+}
+export function serialize(asyncFunction) {
+    let lastRun = Promise.resolve();
+    function wrapper(...args) {
+        return lastRun = new Promise((res, rej) => {
+            after(lastRun, () => {
+                asyncFunction.apply(this, args).then(res, rej);
+            });
+        });
+    }
+    wrapper.after = function () {
+        return lastRun = new Promise((res, rej) => { after(lastRun, res); });
+    };
+    return wrapper;
+}
