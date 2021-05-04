9228700fde7585fb71eef4a839b188c7e2ed7447
Justin Bennett <zephraph@gmail.com>
2021-05-03 22:38:25 -0400

    Directly assume method wrapper is exact mapped property of O

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,7 @@
 declare type uninstaller = () => void;
-declare type anyFunction = (...args: any[]) => any;
 declare type methodWrapperFactory<T extends Function> = (next: T) => T;
-export declare function around<O extends Record<string, unknown>, T extends anyFunction>(obj: O, factories: {
-    [key in keyof O | string]: methodWrapperFactory<T>;
+export declare function around<O extends Record<string, any>>(obj: O, factories: {
+    [key in keyof O]: methodWrapperFactory<O[key]>;
 }): uninstaller;
 export declare function after(promise: Promise<any>, cb: () => void): Promise<void>;
 export declare function serialize(asyncFunction: Function): {
