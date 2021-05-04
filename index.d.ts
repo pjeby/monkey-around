513c2b15d363ca6ccdf13a901e78e454bca06fea
Justin Bennett <zephraph@gmail.com>
2021-05-03 22:21:58 -0400

    Further tweaking to allow functions not in O

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
 declare type uninstaller = () => void;
+declare type anyFunction = (...args: any[]) => any;
 declare type methodWrapperFactory<T extends Function> = (next: T) => T;
-export declare function around<O extends Record<string, any>, T extends Function>(obj: O, factories: {
-    [key in keyof O]: methodWrapperFactory<T>;
+export declare function around<O extends Record<string, unknown>, T extends anyFunction>(obj: O, factories: {
+    [key in keyof O | string]: methodWrapperFactory<T>;
 }): uninstaller;
 export declare function after(promise: Promise<any>, cb: () => void): Promise<void>;
 export declare function serialize(asyncFunction: Function): {
