5845f6fd6745a9423f7c81fe902943882354623b
Justin Bennett <zephraph@gmail.com>
2021-05-03 21:53:16 -0400

    Add mapped type to avoid inferring T as a union

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare type uninstaller = () => void;
 declare type methodWrapperFactory<T extends Function> = (next: T) => T;
-export declare function around<T extends Function>(obj: Object, factories: {
-    [key: string]: methodWrapperFactory<T>;
+export declare function around<O extends Record<string, any>, T extends Function>(obj: O, factories: {
+    [key in keyof O]: methodWrapperFactory<T>;
 }): uninstaller;
 export declare function after(promise: Promise<any>, cb: () => void): Promise<void>;
 export declare function serialize(asyncFunction: Function): {
